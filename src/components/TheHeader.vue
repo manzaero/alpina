005950898f2ddd60b6f76987e0c88d30625ea8f3
manzaero <ligoero@gmail.com>
2023-02-28 19:26:04 +0500

    start project

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container text-center container__font">
+    <div class="row">
+      <div class="col">
+        <div class="row">
+          <div class="col">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Санкт-Питербург
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+          </div>
+          <div class="col">
+            <a href="tel:88007004024">8 800 700 40 24</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-6">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Позвонить или написать
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="col">
+        <div class="row">
+        <div class="col">
+          <a href="#">О нас</a>
+        </div>
+        <div class="col">
+          <a href="#">Наши салоны</a>
+        </div>
+        <div class="col">
+          <a href="#">Оплата и доставка</a>
+        </div>
+        <div class="col">
+          <a href="#">Для бизнеса</a>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="row">
+          <div class="col">
+            <a href="">Каталог</a>
+          </div>
+          <div class="col">
+            <a href="">Комнаты</a>
+          </div>
+          <div class="col">
+            <a href="">Дизайн-проекты</a>
+          </div>
+          <div class="col">
+            <a href="">Мебель на заказ</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-6">
+        <img src="../assets/image/logo.png" alt="">
+      </div>
+      <div class="col">
+        3 of 3
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        1
+      </div>
+      <div class="col">
+        2
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <content>
+
+  </content>
+  <footer>
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheMain"
+}
+</script>
+
+<style scoped>
+
+</style>
