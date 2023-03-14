1126a96ac8038e743c16134cfbf7ac9a700582a5
manzaero <ligoero@gmail.com>
2023-03-14 17:23:56 +0500

    start is coding TheOffer.vue

---
+++ src/components/TheOffer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="d-flex justify-content-between">
+      <div class="d-flex__catalog">Популярные категории</div>
+      <a href="#" class="d-flex__all">
+        <p class="d-flex__products">Все категории</p>
+        <div class="d-flex__line"/>
+      </a>
+    </div>
+    <div class="row row-cols-4">
+      <div class="col">
+        <div class="img">1</div>
+        <p>Декоративный куст, с шикарными листьями, горшком</p>
+        <span>120 × 212  × 46 </span>
+      </div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheOffer"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
   <the-catalog/>
   <the-category :categories="categories"/>
   <the-furniture/>
+  <the-offer/>
 </template>
 
 <script>
@@ -10,6 +11,7 @@ import TheCategory from "@/components/TheCategory";
 import TheTitle from "@/components/TheTitle";
 import TheCatalog from "@/components/catalog/TheCatalog";
 import TheFurniture from "@/components/furniture/TheFurniture";
+import TheOffer from "@/components/TheOffer";
 import {catValue} from "@/const/data-value";
 export default {
   name: 'HomeView',
@@ -22,7 +24,8 @@ export default {
     TheFurniture,
     TheCatalog,
     TheTitle,
-    TheCategory
+    TheCategory,
+    TheOffer
   }
 }
 </script>
