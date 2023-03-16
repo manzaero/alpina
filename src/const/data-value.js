abcbf334fe45e71e5e2a6d3d3ef1739ac1491eb1
manzaero <ligoero@gmail.com>
2023-03-16 19:41:26 +0500

    refact code is v-for in TheOffers.vue component

--- src/const/data-value.js
+++ src/const/data-value.js
@@ -114,3 +114,118 @@ export const slides = [
         img: 'tyumen'
     }
 ]
+export const offers = [
+    {
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'divan2',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: 'В корзине'
+    },{
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'divan3',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: ''
+    },{
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'kreslo',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: 'В корзине'
+    },{
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'kreslo2',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: ''
+    },{
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'none',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: 'В корзине'
+    },{
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'puff',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: 'В корзине'
+    },{
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'ruchka',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: 'В корзине'
+    },{
+        id: 1,
+        name: 'Декоративный куст, с шикарными листьями, горшком',
+        path: '/',
+        png: 'heart',
+        size: '120 × 212  × 46 ',
+        img: 'tumba',
+        price: '2 000',
+        oldPrice:'2 300 000 руб.',
+        sale: 'Распродажа',
+        icon1: 'bush',
+        icon2: 'bush2',
+        icon3: 'bush3',
+        cart: ''
+    }
+]
