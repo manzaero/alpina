673b5afa177ce8429b4d022248ba18b244501815
manzaero <ligoero@gmail.com>
2023-04-06 09:06:18 +0500

    refact contact.vue

---
+++ src/components/contacts/TheList.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="contacts__list">
+    <div class="contacts__card">
+      <div class="contacts__street">
+        <div class="contacts__name">Пермякова, 82</div>
+        <a href="#" class="contacts__route">маршрут</a>
+      </div>
+      <div class="contacts__offers">
+        <div>
+          <img src="../../assets/image/contacts/clock.png" alt="" class="contacts__clock-icon">
+        </div>
+        <div class="contacts__hour">
+          <div class="contacts__today">Сегодня с 10:00 до 19:00</div>
+          <div class="contacts__open">Открыто</div>
+        </div>
+      </div>
+      <div class="contacts__call">
+        <div>
+          <img src="../../assets/image/contacts/tel.png" alt="" class="contacts__tel-icon">
+        </div>
+        <div class="contacts__phone">
+          <div class="contacts__tel-number">+7 (3452) 54‒61‒62</div>
+          <a href="tel:+73452546162" class="contacts__calling">Позвонить</a>
+        </div>
+      </div>
+      <div class="contacts__mailto">
+        <div>
+          <img src="../../assets/image/contacts/mail.png" alt="" class="contacts__mail-icon">
+        </div>
+        <div class="contacts__mail">
+          <div class="contacts__mail-name">Dryppa90@gmail.com</div>
+          <a href="mailto:dryppa90@gmail.com" class="contacts__write">Написать</a>
+        </div>
+      </div>
+    </div>
+    <div class="contacts__card">
+      <div class="contacts__street">
+        <div class="contacts__name">Пермякова, 82</div>
+        <a href="#" class="contacts__route">маршрут</a>
+      </div>
+      <div class="contacts__offers">
+        <div>
+          <img src="../../assets/image/contacts/clock.png" alt="" class="contacts__clock-icon">
+        </div>
+        <div class="contacts__hour">
+          <div class="contacts__today">Сегодня с 10:00 до 19:00</div>
+          <div class="contacts__open">Открыто</div>
+        </div>
+      </div>
+      <div class="contacts__call">
+        <div>
+          <img src="../../assets/image/contacts/tel.png" alt="" class="contacts__tel-icon">
+        </div>
+        <div class="contacts__phone">
+          <div class="contacts__tel-number">+7 (3452) 54‒61‒62</div>
+          <a href="tel:+73452546162" class="contacts__calling">Позвонить</a>
+        </div>
+      </div>
+      <div class="contacts__mailto">
+        <div>
+          <img src="../../assets/image/contacts/mail.png" alt="" class="contacts__mail-icon">
+        </div>
+        <div class="contacts__mail">
+          <div class="contacts__mail-name">Dryppa90@gmail.com</div>
+          <a href="mailto:dryppa90@gmail.com" class="contacts__write">Написать</a>
+        </div>
+      </div>
+    </div>
+    <div class="contacts__card">
+      <div class="contacts__street">
+        <div class="contacts__name">Пермякова, 82</div>
+        <a href="#" class="contacts__route">маршрут</a>
+      </div>
+      <div class="contacts__offers">
+        <div>
+          <img src="../../assets/image/contacts/clock.png" alt="" class="contacts__clock-icon">
+        </div>
+        <div class="contacts__hour">
+          <div class="contacts__today">Сегодня с 10:00 до 19:00</div>
+          <div class="contacts__open">Открыто</div>
+        </div>
+      </div>
+      <div class="contacts__call">
+        <div>
+          <img src="../../assets/image/contacts/tel.png" alt="" class="contacts__tel-icon">
+        </div>
+        <div class="contacts__phone">
+          <div class="contacts__tel-number">+7 (3452) 54‒61‒62</div>
+          <a href="tel:+73452546162" class="contacts__calling">Позвонить</a>
+        </div>
+      </div>
+      <div class="contacts__mailto">
+        <div>
+          <img src="../../assets/image/contacts/mail.png" alt="" class="contacts__mail-icon">
+        </div>
+        <div class="contacts__mail">
+          <div class="contacts__mail-name">Dryppa90@gmail.com</div>
+          <a href="mailto:dryppa90@gmail.com" class="contacts__write">Написать</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheList"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="container">
-    <h1>Contact</h1>
+    <div class="contacts">
+      <div class="navigation-page">
+        <span>
+          <router-link to="/">Главная</router-link>
+          /
+          <a href="">Наши салоны</a>
+        </span>
+      </div>
+      <div class="contacts__title">
+        Наши салоны
+      </div>
+      <div class="contacts__button-city">
+        <button class="btn">Тюмень</button>
+        <button class="btn">Челябинск</button>
+        <button class="btn">Москва</button>
+        <button class="btn">Сыктывкар</button>
+        <button class="btn">Сочи</button>
+      </div>
+      <div class="contacts__way">
+        <div class="contacts__way-position">
+          <a class="contacts__way-list" href="">Списком</a>
+          <div class="contacts__list-border"></div>
+        </div>
+        <div class="contacts__map-position">
+          <a class="contacts__way-map" href="">На карте</a>
+          <div class="contacts__map-border"></div>
+        </div>
+      </div>
+      <hr>
+    </div>
+    <the-list/>
   </div>
 </template>
 
 <script>
+import TheList from "@/components/contacts/TheList";
 export default {
-  name: "ContactsView"
+  name: "ContactsView",
+  components:{TheList}
 }
 </script>
 
